4a5cecee2dad172a3249d614aa0886cf9e694853
Leszek Mielnikow <63610219+LexiestLeszek@users.noreply.github.com>
2024-02-11 10:49:53 +0100

    more comments added

--- namegen.py
+++ namegen.py
@@ -1,5 +1,4 @@
 import torch
-# Import the PyTorch library
 
 class NameGen:
     def __init__(self):
@@ -7,29 +6,42 @@ class NameGen:
         self.stoi = None
         self.itos = None
         self.fourgrams = None
-    # Define the NameGen class with initialization of attributes
+    # Defining the NameGen class with initialization of attributes
 
     def load_and_train(self, filename):
-        # Load dataset and prepare character mappings
+        # Load dataset and train the model
+        
         with open(filename, 'r') as f:
             words = f.read().splitlines()
         # Open and read the file to get a list of words
 
         self.characters = sorted(list(set(''.join(words))))
-        # Get and sort unique characters in the dataset (basically alphabet)
+        # Create vocabulary - get and sort unique characters in the dataset (basically alphabet)
         vocab_len = len(self.characters) + 1
-        # Calculate the vocabulary length (characters + dot)
-        self.stoi = {s: i + 1 for i, s in enumerate(self.characters)}
+        # Calculate the vocabulary length (characters) + dot
+        print("Vocab : ", self.characters)
+        
+        self.stoi = {}
+        for i, s in enumerate(self.characters):
+            self.stoi[s] = i +  1
+        # Create character-to-index
+        
         self.stoi['.'] = 0
-        self.itos = {i: s for s, i in self.stoi.items()}
-        # Create character-to-index and index-to-character mappings
+        # The dot represents market for the start and end of a name
+        
+        self.itos = {}
+        for s, i in self.stoi.items():
+            self.itos[i] = s
+        # Create index-to-character mappings
 
         self.fourgrams = torch.zeros((vocab_len, vocab_len, vocab_len, vocab_len), dtype=torch.int32)
         # Initialize a tensor to store frequency of four characters occurring together
         for word in words:
             chs = ['.', '.', '.'] + list(word) + ['.', '.', '.']
             # Add padding dots to the word
-            # dots act as markers indicating the start and end of words
+            # Three dots act as markers indicating the start and end of words
+            # They are added as placeholders to represent the absence of a character at the beginning and end of a word
+            # They are called sentinel characters - special symbols that mark the beginning or end of a sequence
             for ch1, ch2, ch3, ch4 in zip(chs, chs[1:], chs[2:], chs[3:]):
                 ix1 = self.stoi[ch1]
                 ix2 = self.stoi[ch2]
@@ -37,16 +49,19 @@ class NameGen:
                 ix4 = self.stoi[ch4]
                 self.fourgrams[ix1, ix2, ix3, ix4] += 1
         # Populate the fourgrams tensor with frequencies
-        # self.fourgrams[1][2][3][4] would probably contain the frequency of occurrence of the 'abcd' sequence 
+        # self.fourgrams[1][2][3][4] would probably contain the frequency of occurrence of the 'abcd' sequence
 
         #torch.save(self.fourgrams, "namegen_weights.pt")
         # Save the trained model weights
 
     def generate_names(self, num_words=1):
+        # Passing the number of words to generate
+        
         for _ in range(num_words):
             name = []
             ix1 = ix2 = ix3 = 0
             # Initialize indices for generating names
+            
             while True:
                 p = self.fourgrams[ix1, ix2, ix3].float()
                 p = p / p.sum()
@@ -54,6 +69,7 @@ class NameGen:
 
                 probs_flat = p.view(-1)
                 # Collapse all dimensions into a single dimension
+                
                 adjusted_ix = torch.multinomial(probs_flat, num_samples=1)
                 # Randomly sample the next character index based on the input probabilities provided
 
@@ -79,4 +95,3 @@ class NameGen:
 model = NameGen()
 model.load_and_train('female_names_rus.txt')
 model.generate_names(num_words=10)
-# Create an instance of NameGen, load data and train on a dataset, then generate names
